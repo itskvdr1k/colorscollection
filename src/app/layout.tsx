eb196c2ed214bbb46926d5d0f0e3ec8bb619855f
itskvdr1k <nikitafleshmob@gmail.com>
2023-12-23 14:43:11 +0300

    seo optimization added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,14 @@ export const metadata: Metadata = {
   title: 'Color Palette',
   description: 'Color Palette is a tool, helping you to choose you favority color',
   keywords: "color, palette, rgb",
-  authors: [{name: "kvdr1k", url: "https://instagram.com/kvdr1k"}]
+  authors: [{name: "kvdr1k", url: "https://instagram.com/kvdr1k"}],
+  verification: {
+    google: "zpCW_D1k8wBVNT0gqY0w2OGcyhojRU4-EN6tuiSy-g8"
+  },
+  robots: {
+    index: true,
+    follow: true
+  }
 }
 
 export default function RootLayout({
